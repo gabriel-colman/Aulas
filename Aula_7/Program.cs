08ea6917422e35579d2dae2dd9bd77d921dfd833
Gabril Colman <gabriecolman7@gmail.com>
2024-11-04 14:54:05 -0400

    Alteracao

--- Aula_7/Program.cs
+++ Aula_7/Program.cs
@@ -34,6 +34,33 @@ namespace Aula_7
                     }
                 }
             }
+
+            // Acessar um elemento da matrizTri
+            Console.WriteLine(matriz3D[1, 1, 1]);
+            Console.WriteLine(matriz3D[0, 0, 0]);
+
+            // Alterar o valor de um elemento da matrizTri
+            matriz3D[1, 1, 1] = 100;
+            Console.WriteLine(matriz3D[1, 1, 1]);
+
+            // Somar todos que tem dentro da matrizTri
+            int soma = 0;
+            for (int i = 0; i < matriz3D.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz3D.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matriz3D.GetLength(2); k++)
+                    {
+                        soma += matriz3D[i, j, k];
+                    }
+                }
+            }
+
+            // Acessar todos os elementos da matrizTri com Foreach
+            foreach (int item in matriz3D)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
