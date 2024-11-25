1fd76cd89070635ad5ca8a7233c152f308d1724e
Gabril Colman <gabriecolman7@gmail.com>
2024-11-25 16:40:25 -0400

    Pilha encadeada

--- Aula_14/obj/Debug/net8.0/Aula_14.AssemblyInfo.cs
+++ Aula_14/obj/Debug/net8.0/Aula_14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55641ab94ee40df58e0583eff3842fd99095b448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70c8c67e5d316ed000a67e8476dbde7f3fe5dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
