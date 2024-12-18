9e90873148706b34790b53c287e7b7d07f8e6819
Gabril Colman <gabriecolman7@gmail.com>
2024-12-18 14:41:20 -0400

    Relacao

--- Aula_20_OO_Relacoes/obj/Debug/net8.0/Aula_20_OO_Relacoes.AssemblyInfo.cs
+++ Aula_20_OO_Relacoes/obj/Debug/net8.0/Aula_20_OO_Relacoes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_20_OO_Relacoes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c93c8f69fe7f5ef54a84438748b7b7dba23bcd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e92069efbdf1375f095799428a9d90e83beb71ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_20_OO_Relacoes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_20_OO_Relacoes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
